7e7e47942371ee95446def6d6daa09dbf9cc6129
vladimir lozhnikov <vladimir.lozhnikov@gmail.com>
2020-02-13 18:23:50 +0300

    initical commit

---
+++ fTrafficCore/Types.fs
@@ -0,0 +1,17 @@
+﻿namespace fTrafficCore
+
+module Types =
+
+    type Position = { x: float; y: float; z: float }
+
+    type Node =
+        | Empty
+        | CrossRoad
+        | TrafficLight
+        | Car
+
+    type Point = { p: Position; t: Node }
+
+    type Road = { points: Point list }
+
+    type RoadNetwork = { roads: Road list }
