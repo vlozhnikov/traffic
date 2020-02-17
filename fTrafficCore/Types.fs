0fa6441791eb9b0f1689008ab39e925d761b9751
vladimir lozhnikov <vladimir.lozhnikov@gmail.com>
2020-02-17 19:28:08 +0300

    roadcorss added

--- fTrafficCore/Types.fs
+++ fTrafficCore/Types.fs
@@ -21,9 +21,12 @@ module Types =
 
     // type of the node on the map. uses inside point
     type Node =
-        | Coordinate
-        | CrossRoad
-        | TrafficLight
+        | BEGIN // start point of road (optional)
+        | END // end point of road(optional)
+        | BREAK // break of road (optional)
+        | CONTINUE // continue of road (optional)
+        | COORDINATE
+        | TRAFFICLIGHT
 
     // point on the map. uses inside roads, etc
     type Point = { p: Position; t: Node list }
@@ -51,13 +54,29 @@ module Types =
                 { n=r.n; pl=r.pl @ [po] }
 
             // static member finds public crossroads of road1 and road2
-            static member CrossRoads r1 r2 =
+            static member CrossRoads2 r1 r2 =
 
                 r1.pl
                 |> List.filter (fun c1 -> match (c1.p, r2.pl) with
                                           | Includes(_) -> true
                                           | _ -> false
                                )
+
+            static member CrossRoads (roads: Road list) =
+
+                let rec inCross r roads res = 
+                    match roads with
+                    | h::t when h.n <> r.n ->
+                        let nextRes = Seq.append res (Road.CrossRoads2 r h)
+                        inCross r t nextRes
+                    | _::t ->
+                        inCross r t res
+                    | [] -> res
+
+                roads
+                |> Seq.map (fun x -> inCross x roads Seq.empty)
+                |> Seq.collect (fun r -> r)
+                |> Seq.distinct
 
             // returns total length of road
             // sqrt((x2 - x2)^2 + (y2 - y1)^2 + (z2 - z1)^2)
@@ -71,7 +90,7 @@ module Types =
                         let nextLength = l + sqrt(pown (h.p.x - nextHead.p.x) 2 + pown (h.p.y - nextHead.p.y) 2 + pown (h.p.z - nextHead.p.z) 2)
                         inLength t nextLength
 
-                let coordinates = List.filter (fun x -> List.contains Coordinate x.t) r.pl
+                let coordinates = List.filter (fun x -> List.contains COORDINATE x.t) r.pl
                 inLength coordinates 0.
 
             // try find point on the road by position
--- traffic/Program.fs
+++ traffic/Program.fs
@@ -7,22 +7,76 @@ open fTrafficCore.Types
 let main argv =
     printfn "Hello World from F#!"
 
-    let road1 =
-        road "road1" [
-                        point (6., 0., 0.) [Coordinate; CrossRoad];
-                        point (9., 0., 0.) [Coordinate; TrafficLight];
-                        point (9., 6., 0.) [Coordinate; CrossRoad];
+    let rh1 =
+        road "rh1" [
+                        point (1., 7., 0.) [BEGIN; COORDINATE];
+                        point (3., 7., 0.) [COORDINATE];
+                        point (5., 7., 0.) [COORDINATE];
+                        point (7., 7., 0.) [COORDINATE];
+                        point (8., 7., 0.) [COORDINATE; TRAFFICLIGHT; END];
                     ]
 
-    let road2 =
-        road "road2" [
-                        point (6., 0., 0.) [Coordinate; CrossRoad];
-                        point (9., 1., 0.) [Coordinate];
-                        point (9., 6., 0.) [Coordinate; TrafficLight; CrossRoad];
+    let rh2 =
+        road "rh2" [
+                        point (1., 5., 0.) [BEGIN; COORDINATE];
+                        point (3., 5., 0.) [COORDINATE];
+                        point (5., 5., 0.) [COORDINATE];
+                        point (7., 5., 0.) [COORDINATE];
+                        point (8., 5., 0.) [COORDINATE; TRAFFICLIGHT; END];
                     ]
 
-    let crossrods = road1.TryFindPoint (position (6., 1., 0.))
+    let rh3 =
+        road "rh3" [
+                        point (1., 3., 0.) [BEGIN; COORDINATE];
+                        point (3., 3., 0.) [COORDINATE];
+                        point (5., 3., 0.) [COORDINATE];
+                        point (7., 3., 0.) [COORDINATE];
+                        point (8., 3., 0.) [COORDINATE; TRAFFICLIGHT; END];
+                    ]
 
-    printfn "road length road1 %f, road2 %f" road1.Length road2.Length
+    let rh4 =
+        road "rh4" [
+                        point (1., 1., 0.) [BEGIN; COORDINATE];
+                        point (3., 1., 0.) [COORDINATE];
+                        point (5., 1., 0.) [COORDINATE];
+                        point (7., 1., 0.) [COORDINATE];
+                        point (8., 1., 0.) [COORDINATE; TRAFFICLIGHT; END];
+                    ]
+
+    let rv1 =
+        road "rv1" [
+                        point (3., 8., 0.) [BEGIN; COORDINATE];
+                        point (3., 7., 0.) [COORDINATE];
+                        point (3., 5., 0.) [COORDINATE];
+                        point (3., 3., 0.) [COORDINATE];
+                        point (3., 1., 0.) [COORDINATE];
+                        point (3., 0., 0.) [END; COORDINATE; TRAFFICLIGHT];
+                    ]
+
+    let rv2 =
+        road "rv2" [
+                        point (5., 8., 0.) [BEGIN; COORDINATE];
+                        point (5., 7., 0.) [COORDINATE];
+                        point (5., 5., 0.) [COORDINATE];
+                        point (5., 3., 0.) [COORDINATE];
+                        point (5., 1., 0.) [COORDINATE];
+                        point (5., 0., 0.) [END; COORDINATE; TRAFFICLIGHT];
+                    ]
+
+    let rv3 =
+        road "rv3" [
+                        point (7., 8., 0.) [BEGIN; COORDINATE];
+                        point (7., 7., 0.) [COORDINATE];
+                        point (7., 5., 0.) [COORDINATE];
+                        point (7., 3., 0.) [COORDINATE];
+                        point (7., 1., 0.) [COORDINATE];
+                        point (7., 0., 0.) [END; COORDINATE; TRAFFICLIGHT];
+                    ]
+
+    let length = rh1.Length
+    let crossrods = Road.CrossRoads [rh1; rh2; rh3; rh4; rv1; rv2; rv3]
+
+    crossrods
+    |> Seq.iter (fun x -> printfn "position x: %f, y: %f" x.p.x x.p.y)
 
     0 // return an integer exit code
